bdd2bd389cb474254bce9741fb39bef271f1f778
Daniel Elliot <danielelliotr@gmail.com>
2021-01-28 14:49:06 -0800

    js logic to add active class to search el on click

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active')
+  input.focus()
+})
